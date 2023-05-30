1a2bd724dd82ca80c3f376363f7960d36ce6e76f
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-05-30 20:34:40 -0300

    Search Route Form Layout

--- src/routes/Home/Search/index.tsx
+++ src/routes/Home/Search/index.tsx
@@ -1,7 +1,19 @@
+import Button from '../../../components/Button';
 import './styles.css';
 
-export default function Search(){
-    return(
-        <h1>oi</h1>
+export default function Search()
+{
+    return (
+        <main>
+            <div className="container">
+                <div className="form-container">
+                    <h1>Encontre um perfil Github</h1>
+                    <form>
+                        <input type="text" name="profileName" placeholder="Usuário GitHub" />
+                    </form>
+                    <Button text="Encontrar" />
+                </div>
+            </div>
+        </main>
     );
 } 
